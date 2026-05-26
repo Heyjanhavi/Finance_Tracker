f258a042060ce907af0ee8baf5af0c281cd27f83
Janhavi Kolhe <kolhejnhvi@gmail.com>
2026-05-26 22:33:55 +0530

    fixed import path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { FinanceTracker } from "@/components/temp";
+import { FinanceTracker } from "@/components/Financetracker";
 
 export const Route = createFileRoute("/")({
   component: FinanceTracker,
